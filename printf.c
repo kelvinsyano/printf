2131dd3bf5a18ecf7991d5ebd9527b5d1ca551d5
kelvinsyano <wbilly29@yahoo.com>
2023-03-28 21:43:25 -0700

    dealing with binary

--- printf.c
+++ printf.c
@@ -7,8 +7,16 @@ int _printf(const char *format, ...)
 {
 	int num_printed;
 	va_list args;
+	int printed_chars;
+	char buffer[1024];
+	int i, num;
+	unsigned int binary_num;
 
 	var_start(args, format);
+	printed_chars = 0;
+	binary_num = 0;
+	i = 0;
+	num = 0;
 
 	while (format && format[i])
 	{
@@ -31,6 +39,10 @@ int _printf(const char *format, ...)
 					num = va_arg(args, int);
 					printed_chars += sprintf(&buffer[printed_chars], "%d", num);
 					break;
+				case 'b':
+					binary_num = va_arg(args, unsigned int);
+					printed_chars += print_binary(&buffer[printed_chars], binary_num);
+					break;
 				default:
 					buffer[printed_chars++] = '%';
 					if (format[i] != '\0')
@@ -47,4 +59,18 @@ int _printf(const char *format, ...)
 		va_end(args);
 		fwrite(buffer, 1, printed_chars, stdout);
 		return (printed_chars);
+}
+/**
+ * print_binary - converts unsigned int to binary
+ * @buffer: pointer to buffer
+ * @num: unsigned int
+ *
+ * Return: returns no of chars
+ */
+int print_binary(char *buffer, unsigned int num)
+{
+	if (num / 2)
+		print_binary(buffer, num / 2);
+	*buffer = (num % 2) + '0';
+	return (1);
 }
